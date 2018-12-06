d45062047e1025b337521ef48a7a9fdea05a8f64
Richard Chance <richard.chance@shockoe.com>
2018-12-06 14:44:19 -0600

    Loading Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+Map _data;
+List _features; //features objects list in Json
+void main() async {
+  _data = await getQuakes();
+  _features = _data['features'];
+  //print(_data['features'] [0] ['properties']);
+
+  runApp(new MaterialApp(
+    title: "Quakes",
+    home: new Quakes(),
+  ));
+}
+
+class Quakes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('Quakes'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      //body of application
+      body: new Center(
+        child: new ListView.builder(
+            itemCount: _features.length,
+            padding: const EdgeInsets.all(15.0),
+            itemBuilder: (BuildContext context, int position) {
+              //creating the roes for our listView
+              if (position.isOdd) return new Divider();
+              final index = position ~/
+                  2; //we are dividing position by 2 and returning an integer result
+              var format = new DateFormat.yMMMd("en_US")
+                  .add_jm(); // formatting the yMMMd into readable format
+              var date = format.format(new DateTime.fromMicrosecondsSinceEpoch(
+                  _features[index]['properties']['time'] * 1000,
+                  isUtc: true));
+
+              return new ListTile(
+                title: new Text(
+                  "At: $date",
+                  style: new TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.w500),
+                ),
+                subtitle: new Text(
+                  "${_features[index]['properties']['place']}",
+                  style: new TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey,
+                      fontStyle: FontStyle.italic),
+                ),
+                leading: new CircleAvatar(
+                  backgroundColor: Colors.red,
+                  child: new Text(
+                    "${_features[index]['properties']['mag']}",
+                    style: new TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontStyle: FontStyle.normal),
+                  ),
+                ),
+                onTap: () {
+                  _showAlertMessage(
+                      context, "${_features[index]['properties']['title']}");
+                },
+              );
+            }),
+      ),
+    );
+  }
+
+  //alert dialog when you click on listView item
+  void _showAlertMessage(BuildContext context, String message) {
+    var alert = new AlertDialog(
+      title: new Text('Earthquake'),
+      content: new Text(message),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: new Text('Close'))
+      ],
+    );
+    //show the alert dialog
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
+}
+
+//https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
+Future<Map> getQuakes() async {
+  String apiUrl =
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body);
+}
